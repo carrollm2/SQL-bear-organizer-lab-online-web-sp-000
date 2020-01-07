942c9c6c8445f881bf68bd0d5ad9718648186f33
Michael Carroll <carrollm2@gmail.com>
2020-01-07 04:48:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  color_group_counts =  'SELECT color, COUNT(color) FROM bears GROUP BY color'
+  return 'SELECT color, COUNT(color) FROM bears GROUP BY color'
 
-  return 'SELECT color, COUNT(color) FROM bears WHERE color == highest_count_color'
+  # return 'SELECT color, COUNT(color) FROM bears WHERE color == highest_count_color'
 end
 
 def counts_number_of_bears_with_goofy_temperaments
